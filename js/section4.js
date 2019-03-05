8b676eb7227268061759cf64ea3b1186758f1cbc
YadPe <petitot.py@gmail.com>
2019-03-05 16:59:37 +0100

    Added video + canvas to section 4

---
+++ js/section4.js
@@ -0,0 +1,34 @@
+// SECTION 4 // 
+// Amiibo pres Tutotrial //
+
+var sec4Canvas = document.getElementById('section4Canvas');
+var ctx    = sec4Canvas.getContext('2d');
+var videoTuto  = document.getElementById('tutoVideo');
+
+
+sec4Canvas.width = window.innerWidth;
+sec4Canvas.height = window.innerHeight;
+
+//var hRatio = (sec4Canvas.width / videoTuto.videoWidth) * videoTuto.videoHeight;
+
+videoTuto.addEventListener('play', function () {
+    var $this = this; //cache
+    (function loop() {
+        if (!$this.paused && !$this.ended) {
+
+            //ctx.drawImage($this, 0, 0);
+            ctx.drawImage($this, 0, 0, sec4Canvas.width, sec4Canvas.width);
+            setTimeout(loop, 1000 / 30); // drawing at 30fps
+        }
+    })();
+}, 0);
+
+$(window).resize(resizeCanvas);
+
+function resizeCanvas(){
+    sec4Canvas.width = window.innerWidth;
+    sec4Canvas.height = window.innerHeight;
+    console.log("Resizing.." + sec4Canvas.width + "x" + sec4Canvas.height);
+
+    //hRatio = (sec4Canvas.width / videoTuto.videoWidth) * videoTuto.videoHeight;
+}
