bf5019c92c60914c7036036235a61bcc707a47c9
yadpe <mwayannis@gmail.com>
2019-03-06 22:49:26 +0100

    test  paralax

---
+++ js/section1.js
@@ -0,0 +1,15 @@
+$("#section1").mousemove(function (e) {
+    parallaxIt(e, "#foreground  ", -100);
+    parallaxIt(e, "#background", -30);
+});
+
+function parallaxIt(e, target, movement) {
+    var $this = $("#section1");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+}
