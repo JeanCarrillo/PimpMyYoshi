2be73fdb28e09fccef2a702b8a67d6a1cef7f356
Marc-Gregoire Balme <marcbalme@yahoo.com>
2019-03-06 10:05:50 +0100

    section 5

--- js/section5.js
+++ js/section5.js
@@ -1,21 +1,12 @@
-let monImage = document.querySelector(".img1");
+//let mesImages = document.getElementsByClassName("imgtop");
 
-monImage.addEventListener('click', function() {
-  let monSrc = monImage.getAttribute('src');
-  if(monSrc === 'topsecret.jpg') {
-    monImage.setAttribute('src', 'yoshibleue.jpeg');
+
+function changeImages(e){
+  let imgId = e.target.id
+  let monImage = document.getElementById(imgId);
+  if(imgId === 'img1') {
+  monImage.setAttribute('src', 'assets/images/yoshibleue.jpeg');
   } else {
-    monImage.setAttribute('src', 'topsecret.jpg');
+  monImage.setAttribute('src', 'assets/images/yoshirose.jpeg');
   }
-});
-
-let monImage = document.querySelector(".img2");
-
-monImage2.addEventListener('click', function() {
-  let monSrc2 = monImage2.getAttribute('src');
-  if (monSrc2 === 'topsecret2.jpg') {
-    monImage2.setAttribute('src', 'yoshirose.jpeg');
-  } else {
-    monImage2.setAttribute('src', 'topsecret2.jpg');
-  }
-});
+}
