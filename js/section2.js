fc65757dc87d182a0772950fe3208c189e572840
eugenie <Vous@exemple.com>
2019-03-06 21:38:23 +0100

    ajustements

--- js/section2.js
+++ js/section2.js
@@ -2,13 +2,19 @@
 $(document).ready(function () 
 {
     color = 'default';
-    accessory = 'none';
+    accessory = 'default';
     //Customization 
     $('.green-button').on({'click': function()
     {
         if (color=='default')
         {
             $('.green-button').removeAttr('id');
+        }
+        if (accessory=='default')
+        {
+            $('#change-image').attr('src','assets/images/yoshiVert.png');
+            $('.title-custom').text('Classic Yoshi');
+            $('.text-custom').text('The orignal green Yoshi for purists.');
         }
         
         if (accessory=='none')
@@ -33,6 +39,12 @@ $(document).ready(function ()
         {
             $('.green-button').removeAttr('id');
         }
+        if (accessory=='default')
+        {
+            $('#change-image').attr('src','assets/images/yoshiRose.png');
+            $('.title-custom').text('Pink Yoshi');
+            $('.text-custom').text('The sweet pink Yoshi for candy floss lovers.');
+        }
         if (accessory=='none')
         {
             $('#change-image').attr('src','assets/images/yoshiRose.png');
@@ -55,6 +67,12 @@ $(document).ready(function ()
         {
             $('.green-button').removeAttr('id');
         }
+        if (accessory=='default')
+        {
+            $('#change-image').attr('src','assets/images/yoshiBleu.png');
+            $('.title-custom').text('Blue Yoshi');
+            $('.text-custom').text('The blue Yoshi for dreamers.');
+        }
         if (accessory=='none')
         {
             $('#change-image').attr('src','assets/images/yoshiBleu.png');
@@ -73,6 +91,16 @@ $(document).ready(function ()
 
     $('.moustache').on({'click': function()
     {
+        if (accessory=='default')
+        {
+            $('.none').removeAttr('id');
+        }
+        if (color == 'default')
+        {
+            $('#change-image').attr('src','assets/images/yoshiVertMoustache.png');
+            $('.title-custom').text('Classic Yoshi with moustache');
+            $('.text-custom').text('The orignal green Yoshi for purists.');    
+        }
         if (color == 'green')
         {
             $('#change-image').attr('src','assets/images/yoshiVertMoustache.png');
@@ -96,6 +124,17 @@ $(document).ready(function ()
 });
     $('.none').on({'click': function()
     {
+        if (accessory=='default')
+        {
+            $('.none').removeAttr('id');
+        }
+
+        if (color == 'default')
+        {
+            $('#change-image').attr('src','assets/images/yoshiVert.png');
+            $('.title-custom').text('Classic Yoshi');
+            $('.text-custom').text('The orignal green Yoshi for purists.');    
+        }
         if (color == 'green')
         {
             $('#change-image').attr('src','assets/images/yoshiVert.png');
